094663bd79cd1cc1d385c060ab8ffe7b8d556763
angelajm6 <angelajaume.m@gmail.com>
2023-04-09 14:00:45 -0400

    fixed error in index.js

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -1,10 +1,27 @@
 // "rafce" to generate a functional component
-import React from 'react'
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import Logo from '../assets/My-logo.png';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'> 
+      <div>
+        <img src={Logo} alt="my logo image" style={{width: '50px'}}></img>
+      </div>
+      <div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+      </div>
+      </div>
+      
   )
 }
 
-export default Navbar
+export default Navbar;
