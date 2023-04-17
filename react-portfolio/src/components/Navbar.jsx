b5053061c152573119d622b319aac3bbbd56bf02
angelajm6 <angelajaume.m@gmail.com>
2023-04-17 13:15:25 -0400

    adding styling to Navbar

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -1,24 +1,48 @@
 // "rafce" to generate a functional component
-import React from 'react';
+import React, {useState} from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import Logo from '../assets/My-logo.png';
 
 
 const Navbar = () => {
+const [nav,setNav] = useState (false)
+const handleClick = () => setNav (!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'> 
       <div>
-        <img src={Logo} alt="my logo image" style={{width: '50px'}}></img>
+        <img src={Logo} alt="my logo image" style={{width: '50px'}}/>
       </div>
-      <div>
-        <ul>
+
+      {/* main menu */}
+        <ul className='hidden md:flex'>
             <li>Home</li>
             <li>About</li>
             <li>Skills</li>
             <li>Work</li>
             <li>Contact</li>
         </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <Fabars/> : <FaTimes/>}
       </div>
+
+      {/* Mobile menu */}
+      <ul className={!nav ? 'hidden' : 'hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Skills</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contact</li>
+      </ul>
+
+      {/* Social icons*/}
+      <div className='hidden'></div>
+
+
+
+
       </div>
       
   )
