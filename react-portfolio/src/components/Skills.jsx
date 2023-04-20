bd8ab6d127c16216f6e614d204fd180e7eb37c53
angelajm6 <angelajaume.m@gmail.com>
2023-04-20 10:33:22 -0400

    imported skills icons

---
+++ react-portfolio/src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HTML from '../assets/HTML5.png';
+import CSS from '../assets/CSS.png';
+import JavaScript from '../assets/JS.png';
+import MySQL from '../assets/MySQL.png';
+import NodeJS from '../assets/NodeJs.png';
+import ReactImg from '../assetsReact.png';
+
+
+const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
