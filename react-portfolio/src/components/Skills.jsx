d903248e83f762003cd959df38f565d309160e63
angelajm6 <angelajaume.m@gmail.com>
2023-04-24 16:03:53 -0400

    finished skills section

--- react-portfolio/src/components/Skills.jsx
+++ react-portfolio/src/components/Skills.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
-import HTML from '../assets/HTML5.png';
-import CSS from '../assets/CSS.png';
-import JavaScript from '../assets/JS.png';
-import MySQL from '../assets/MySQL.png';
-import NodeJS from '../assets/NodeJs.png';
-import ReactImg from '../assetsReact.png';
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
 
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div>
+              <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+              <p className='py-4'>// These are the technologies I've worked with</p>
+          </div>
 
-export default Skills
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                  <p className='my-4'>HTML</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+                  <p className='my-4'>CSS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon" />
+                  <p className='my-4'>JAVASCRIPT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
+                  <p className='my-4'>REACT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Node} alt="Nodejs icon" />
+                  <p className='my-4'>NODE JS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={MySQL} alt="MySQL icon" />
+                  <p className='my-4'>MYSQL</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
